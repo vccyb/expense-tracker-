f5adabd040db37ec8f594fd2f67d4112f209e3a9
vccyb <13170906656@163.com>
2024-06-09 14:53:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+  <button class="btn btn-link">Link</button>
+  <div>{{ counter.count }}</div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "@/store/counter";
+const counter = useCounterStore();
+counter.count++;
+</script>
